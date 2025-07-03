fd988c68494bdd40bc8f93323f2dd94f761fd21b
xyaman <dbarriapaiva@gmail.com>
2025-07-03 13:26:26 +0900

    feat(backend+user): use wrap_parameters

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,19 @@
 class Api::V1::UsersController < ApplicationController
   allow_unauthenticated_access only: %i[ create ]
-  skip_forgery_protection only: [ :create ]
+  wrap_parameters :user, include: [:password, :password_confirmation]
 
   def create
-    user = User.new(params.permit(:email, :username, :password, :password_confirmation))
+    user = User.new(user_params)
     if user.save
       start_new_session_for user
       render json: { user: user.slice(:id, :email, :username) }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
 end
