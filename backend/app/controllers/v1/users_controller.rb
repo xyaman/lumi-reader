1d000b1b5430fb670d5b98496ddce959b709c153
xyaman <dbarriapaiva@gmail.com>
2025-07-04 16:36:10 +0900

    feat(backend+user): add followers and following count to GET `users` response
    - Consider to cache the followers/following count

--- backend/app/controllers/v1/users_controller.rb
+++ backend/app/controllers/v1/users_controller.rb
@@ -22,14 +22,21 @@ class V1::UsersController < ApplicationController
   # @oas_include
   # @tags Users
   # @summary Show a User
-  # @response User found(200) [Hash{user: Hash{id: Integer, email: String, username: String, share_reading_data: Boolean}}]
+  # @response User found(200) [Hash{user: Hash{id: Integer, email: String, username: String, share_reading_data: Boolean, following_count: Integer, followers_count: Integer}}]
   # @response User not found(404) [Hash{error: String}]
   #
   # Returns the user with the given ID.
   def show
     user = User.find_by(id: params[:id])
     if user
-      render json: { user: user.slice(:id, :email, :username, :share_reading_data) }, status: :ok
+
+      following_count = user.following.count
+      followers_count = user.followers.count
+
+      json_user = user.slice(:id, :email, :username, :share_reading_data)
+      json_user.merge!(following_count: following_count, followers_count: followers_count)
+
+      render json: { user: json_user }, status: :ok
     else
       render json: { error: "User not found" }, status: :not_found
     end
--- backend/db/migrate/20250630110207_create_users.rb
+++ backend/db/migrate/20250630110207_create_users.rb
@@ -6,6 +6,10 @@ class CreateUsers < ActiveRecord::Migration[8.0]
       t.string :password_digest, null: false
       t.boolean :share_reading_data, default: true, null: false
 
+      # TODO: Counter cache columns
+      # t.integer :followers_count, default: 0, null: false
+      # t.integer :following_count, default: 0, null: false
+
       t.timestamps
     end
     add_index :users, :email, unique: true
