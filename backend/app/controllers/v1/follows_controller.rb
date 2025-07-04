e6cc6bb221a68f0dd4e0c61c1de984c45044ce6b
xyaman <dbarriapaiva@gmail.com>
2025-07-04 16:17:20 +0900

    refactor(backend): reduce unnecessary code in follows controller

--- backend/app/controllers/v1/follows_controller.rb
+++ backend/app/controllers/v1/follows_controller.rb
@@ -1,5 +1,7 @@
 class V1::FollowsController < ApplicationController
   allow_unauthenticated_access only: %i[ following followers ]
+  before_action :set_user
+  before_action :render_user_not_found, unless: -> { @user }
 
   # @oas_include
   # @tags Follows
@@ -7,12 +9,7 @@ class V1::FollowsController < ApplicationController
   # @response Success(200) [Hash{following: Array<User>}]
   # @response User not found(404) [Hash{error: String}]
   def following
-    user = User.find_by(id: params[:user_id])
-    if user then
-      render json: { following: user.following }
-    else
-      render json: { error: "User not found" }, status: :not_found
-    end
+    render json: { following: @user.following }
   end
 
   # @oas_include
@@ -21,13 +18,7 @@ class V1::FollowsController < ApplicationController
   # @response Success(200) [Hash{followers: Array<User>}]
   # @response User not found(404) [Hash{error: String}]
   def followers
-    puts params
-    user = User.find_by(id: params[:user_id])
-    if user then
-      render json: { followers: user.followers }
-    else
-      render json: { error: "User not found" }, status: :not_found
-    end
+    render json: { followers: @user.followers }
   end
 
   # @oas_include
@@ -35,22 +26,17 @@ class V1::FollowsController < ApplicationController
   # @summary Current user follows another user
   # @response Followed successfully(201) [Hash{message: String}]
   # @response User not found(404) [Hash{error: String}]
-  # @response Can't follow to oneself(422) [Hash{error: String}]
+  # @response Can't follow oneself(422) [Hash{error: String}]
   # @response Already following(422) [Hash{error: String}]
   def create
-    user_to_follow = User.find_by(id: params[:user_id])
-    return render json: { error: "User not found" }, status: :not_found unless user_to_follow
-
-    if Current.user.id == user_to_follow.id
-      return render json: { error: "Can't follow to oneself" }, status: :unprocessable_entity
+    if Current.user.id == @user.id
+      render json: { error: "Can't follow oneself" }, status: :unprocessable_entity
+    elsif Current.user.following.exists?(@user)
+      render json: { error: "Already following" }, status: :unprocessable_entity
+    else
+      Current.user.following << @user
+      render json: { message: "Followed successfully" }, status: :created
     end
-
-    if Current.user.following.include?(user_to_follow)
-      return render json: { error: "Already following" }, status: :unprocessable_entity
-    end
-
-    Current.user.following << user_to_follow
-    render json: { message: "Followed successfully" }, status: :created
   end
 
   # @oas_include
@@ -58,21 +44,26 @@ class V1::FollowsController < ApplicationController
   # @summary Current user unfollows another user
   # @response Unfollowed successfully(200) [Hash{message: String}]
   # @response User not found(404) [Hash{error: String}]
-  # @response Can't unfollow to oneself(422) [Hash{error: String}]
+  # @response Can't unfollow oneself(422) [Hash{error: String}]
   # @response Already not following(422) [Hash{error: String}]
   def destroy
-    user_to_unfollow = User.find_by(id: params[:user_id])
-    return render json: { error: "User not found" }, status: :not_found unless user_to_unfollow
-
-    if Current.user.id == user_to_unfollow.id
-      return render json: { error: "Can't unfollow to oneself" }, status: :unprocessable_entity
+    if Current.user.id == @user.id
+      render json: { error: "Can't unfollow oneself" }, status: :unprocessable_entity
+    elsif !Current.user.following.exists?(@user)
+      render json: { error: "Already not following" }, status: :unprocessable_entity
+    else
+      Current.user.following.delete(@user)
+      render json: { message: "Unfollowed successfully" }, status: :ok
     end
+  end
 
-    if not Current.user.following.include?(user_to_unfollow)
-      return render json: { error: "Already not following" }, status: :unprocessable_entity
-    end
+  private
 
-    Current.user.following.delete(user_to_unfollow)
-    render json: { message: "Unfollowed successfully" }, status: :ok
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def render_user_not_found
+    render json: { error: "User not found" }, status: :not_found
   end
 end
