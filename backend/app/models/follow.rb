be80d39f75601ba183358442c08f3cb9d9609e21
xyaman <dbarriapaiva@gmail.com>
2025-08-28 20:50:04 +0900

    feat(backend+users): add follows-related routes in users_controller
    - add get following, get followers,
    - add put follow, put unfollow
    - add tests

--- backend/app/models/follow.rb
+++ backend/app/models/follow.rb
@@ -4,4 +4,15 @@ class Follow < ApplicationRecord
 
   # The user being followed
   belongs_to :followed, class_name: "User", counter_cache: :followers_count
+
+  validates :follower_id, uniqueness: { scope: :followed_id }
+
+  validate :follower_cannot_follow_self
+
+  private
+  def follower_cannot_follow_self
+    if follower_id == followed_id
+      errors.add(:follower_id, "can't follow yourself")
+    end
+  end
 end
