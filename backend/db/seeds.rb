d1b4e812c542d5808938bdcd74971ac83af084c1
xyaman <dbarriapaiva@gmail.com>
2025-08-29 22:59:53 +0900

    feat(backend): add `is_admin` column to users table

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,8 +1,10 @@
 if ENV["SEED_USERS"]
+  puts "Seeding users..."
+
   users = [
-    { email: "xyaman@lumireader.app", username: "xyaman", plan: "tier1" },
-    { email: "user2@lumireader.app", username: "user2", plan: "free" },
-    { email: "user3@lumireader.app", username: "user3", plan: "free" }
+    { email: "xyaman@lumireader.app", username: "xyaman", is_admin: true },
+    { email: "user2@lumireader.app", username: "user2", is_admin: false },
+    { email: "user3@lumireader.app", username: "user3", is_admin: false }
   ]
 
   users.each do |attrs|
@@ -12,6 +14,7 @@ if ENV["SEED_USERS"]
       user.share_online_status = true
       user.share_presence = true
       user.email_confirmed_at = Time.current
+      user.is_admin = attrs[:is_admin]
     end
   end
 end
