293593c734e5dc0c122edfb114bf8da92a6d7360
xyaman <dbarriapaiva@gmail.com>
2025-08-30 00:23:58 +0900

    feat(backend+patreons): use patreon_id instead of local db id

--- backend/test/controllers/v1/auth_controller_test.rb
+++ backend/test/controllers/v1/auth_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class V1::AuthControllerTest < ActionDispatch::IntegrationTest
-  test "should get patreon" do
-    get v1_auth_patreon_url
-    assert_response :success
-  end
 end
