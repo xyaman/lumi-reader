2eec1e3918997a95c272410fadb7ad13ef886b50
xyaman <dbarriapaiva@gmail.com>
2025-09-14 12:05:56 +0900

    feat(backend+user_avatar): add image validation and resize

--- backend/app/controllers/v1/me_controller.rb
+++ backend/app/controllers/v1/me_controller.rb
@@ -1,4 +1,8 @@
+require "image_processing/mini_magick"
+
 class V1::MeController < ApplicationController
+  before_action :check_file_size, only: [ :avatar ]
+
   # This function is always called when the frontend loads, make sense
   # to update user status here
   def show
@@ -13,21 +17,49 @@ class V1::MeController < ApplicationController
     end
   end
 
-  # TODO: verificar que es una imagen
   def avatar
     unless params[:avatar].present?
       return render_error errors: "Missing avatar file."
     end
 
-    if params[:avatar].size > 2.megabytes
-      return render_error errors: "Avatar is too big (max 2MB)."
+    avatar_file = params[:avatar]
+    user_plan = Current.user.patreon_tier&.name
+
+    # Allowed MIME types
+    basic_types = [ "image/jpeg", "image/png", "image/webp" ]
+    tier_types  = [ "image/gif" ]
+
+    allowed_types = basic_types.dup
+    allowed_types.concat(tier_types) if user_plan == "Supporter"
+
+    unless allowed_types.include?(avatar_file.content_type)
+      return render_error errors: "File type not allowed. Please upload a JPEG, PNG, or WEBP. Patreon supporters can also upload GIFs."
     end
 
-    if Current.user.avatar.attached?
-      Current.user.avatar.purge
+    begin
+      extension = File.extname(avatar_file.original_filename)
+      processed_image = ImageProcessing::MiniMagick
+        .source(params[:avatar])
+        .resize_to_fill(500, 500)
+        .call
+
+      secure_filename = "#{SecureRandom.uuid}#{extension}"
+      Current.user.avatar.purge if Current.user.avatar.attached?
+
+      Current.user.avatar.attach(
+        io: processed_image,
+        filename: secure_filename,
+        content_type: avatar_file.content_type
+      )
+
+      # generate random filename
+      secure_filename = "#{SecureRandom.uuid}#{extension}"
+    rescue
+      return render_error errors: "Invalid or corrupted image file."
+    ensure
+      processed_image&.close!
     end
 
-    Current.user.avatar.attach(params[:avatar])
     if Current.user.avatar.attached?
       avatar_url = url_for(Current.user.avatar)
       render_success data: avatar_url, message: "Avatar updated successfully."
@@ -61,5 +93,17 @@ class V1::MeController < ApplicationController
 
   def user_update_params
     params.expect(user: [ :bio, :share_online_status, :share_presence, :share_reading_sessions ])
+  end
+
+  def check_file_size
+    return true if Current.user&.is_admin?
+
+    content_length = request.headers["Content-Length"].to_i
+    if content_length > 3.megabytes
+      render_error errors: "Request size cannot exceed 3MB.", status: :payload_too_large
+      return false
+    end
+
+    true
   end
 end
