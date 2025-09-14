49c20eb7b2da7286ff7c0d193c0825cb9051a1a4
xyaman <dbarriapaiva@gmail.com>
2025-09-15 01:26:24 +0900

    feat(backend+rack): add too many request middleware to login and register

--- backend/config/initializers/rack_attack.rb
+++ backend/config/initializers/rack_attack.rb
@@ -8,4 +8,24 @@ class Rack::Attack
   self.throttled_responder = lambda do |env|
     [ 429, { "Content-Type" => "application/json" }, [ { status: "error", errors: [ "Too many requests. Please try again later." ] }.to_json ] ]
   end
+
+  throttle("registration/ip", limit: 4, period: 10.minutes) do |req|
+    if req.path == "/v1/registration" && req.post?
+      req.ip
+    end
+  end
+
+  self.throttled_responder = lambda do |env|
+    [ 429, { "Content-Type" => "application/json" }, [ { status: "error", errors: [ "Too many requests. Please try again later." ] }.to_json ] ]
+  end
+
+  throttle("session/ip", limit: 4, period: 10.minutes) do |req|
+    if req.path == "/v1/session" && req.post?
+      req.ip
+    end
+  end
+
+  self.throttled_responder = lambda do |env|
+    [ 429, { "Content-Type" => "application/json" }, [ { status: "error", errors: [ "Too many requests. Please try again later." ] }.to_json ] ]
+  end
 end
